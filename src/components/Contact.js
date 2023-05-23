84603a2558b4c172c412e8b799be153184b953c5
sal-the-coder <jim_nkabinde@icloud.com>
2023-05-23 13:56:55 +0200

    fixing commit error

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form() {
+  const [name, setNamme] = useState('');
+  const [email, setEmail]= useState('')
+  const [number, setNumber]= useState('')
+  const [mesage, setMessage]= useState('')
+
+
+
+
+  const validateEmail = ()=>{
+    console.log(email)
+  }
+
   return (
 
-    <div id = 'contact'>
+    <div id = 'contact' className='contact-form'>
       <h3>Contact</h3>
-        <form >
-            <label for='name'>Full Name</label>
-            <input type='text' id='name'></input>
-
-            <label for='email'>Email Address</label>
-            <input type='email' id='email'></input>
-
-            <label for='number'>Cell Number</label>
-            <input type='text' id='number'></input>
-
+        <form action="https://formspree.io/f/mbjeeden"
+          method="POST">
+          <div>
+          <label htmlFor='name'>Full Name</label>
+            <input type='text' id='name' required name='fname'></input>
+          </div>
+          <div>
+          <label htmlFor='email'>Email Address</label>
+            <input onChange={(e) => setEmail(e.target.value)} onKeyUp={validateEmail()} type='email' id='email' required name='email'></input>
+          </div>
             
-            <label for="message">Message</label>
-            <textarea id='message'></textarea>
-
-            <button>Submit</button>
+          <div>
+          <label htmlFor='number'>Cell Number</label>
+            <input type='tel' id='number' name='cellphone' required ></input>
+          </div>
+           
+           <div>
+            <label htmlFor="message">Message</label>
+            <textarea id='message' required></textarea>
+          </div>
+          <button className='contact-btn'>Submit</button>
 
         </form>
     </div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,14 +5,15 @@ import React from 'react'
 
 const Project = (data) =>{
 return (
-  <>
+  <div key = {data.id}>
+    
     <img src={data.img} alt='my image' height='200px' width = '200px'/>
     <div>{data.title}</div>
     <p>{data.description}</p>
     <a href={data.link}>repo</a>
     <a href={data.site_link}>my site</a>
 
-  </>
+  </div>
 )
 }
 
